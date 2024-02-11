b7e5b3a1498e68a9a2a7d3b9012d9cc03019b323
league <suryanigupta10@gmail.com>
2024-02-10 17:57:31 -0800

    almost finished frogger

---
+++ src/_06_frogger/Car.java
@@ -0,0 +1,10 @@
+package _06_frogger;
+
+public class Car {
+public static void main(String[] args) {
+	int x1 = 20;
+	int y2 = 25;
+	int carSize = 100;
+	
+}
+}
--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -5,7 +5,9 @@ import processing.core.PApplet;
 public class Frogger extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-    
+    int x = 100;
+    int hopDistance;
+    int y = 55;
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -21,10 +23,56 @@ public class Frogger extends PApplet {
     public void draw() {
     background(145,56,49);
     fill(145,56,49);
-    ellipse(100, 0, 200, 300);
+    ellipse(x, y, WIDTH, HEIGHT);
     int speed;
+    display();
     }
-    static public void main(String[] args) {
+    private void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	static public void main(String[] args) {
         PApplet.main(Frogger.class.getName());
     }
+    public void keyPressed()
+    {
+        if(key == CODED){
+            if(keyCode == UP)
+            {
+              y -= 30;
+            }
+            else if(keyCode == DOWN)
+            {
+                y += 20;
+            }
+            else if(keyCode == RIGHT)
+            {
+                x += 20;
+            }
+            else if(keyCode == LEFT)
+            {
+                x -= 20;
+            }
+        }
+       
+    }
+    public void keepBounds() {
+    	
+		if (x < 0) {
+			x = 0;
+		}
+		if (y < 0) {
+			y = 0;
+		}
+		if (width < 800) {
+			width = 800;
+		}
+		if (height < 600) {
+			width = 600;
+		}
+		
+    }
+    
+
 }
